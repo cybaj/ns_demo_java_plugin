eb3baa637616f3485588aca6713884946e3ead15
cybaj <cybaj@naver.com>
2020-06-30 00:08:13 +0900

    add more

--- src/app-version.android.ts
+++ src/app-version.android.ts
@@ -1,5 +1,13 @@
 import { Common } from './app-version.common';
+import { android } from 'tns-core-modules/application';
 
 export class AppVersion extends Common {
+    constructor() {
+        super();
+        const packageManager = android.context.getPackageManager();
+        const packageInfo = packageManager.getPackageInfo(android.context.getPackageName(), 0);
+        const versionName = packageInfo.versionName;
 
+        this.message = `This version number for this package is: ${versionName}`
+    }
 }
